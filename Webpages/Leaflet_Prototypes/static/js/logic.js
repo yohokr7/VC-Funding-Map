fb8f287aeab7b438461d4f264c43177f0a3fd5f0
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-25 14:08:06 -0700

    feat: Finish adding legends to cluster map

--- Webpages/Leaflet_Prototypes/static/js/logic.js
+++ Webpages/Leaflet_Prototypes/static/js/logic.js
@@ -173,13 +173,12 @@ fetch("../../Test_Data/full_table.json").then(response => response.json()).then(
 
     overlayLegendSelector.addEventListener("change", function(e) {
         removeLegend()
-        console.log(e.target.parentElement.children)
-        if (e.target.parentElement.children.item(1).textContent.includes("startupTotalValue")) {
-            addLegend("Total Funding (Magnitude $)", ["Hundreds of Thousands (10^5)",
-                "Millions (10^6)", "Tens of Millions (10^7)", "Hundreds of Millions (10^8)", "Billions (10^9)",
-                "Tens of Billions (10^10)", "Hundreds of Billions (10^11)"], totalFundingScale)
-            e.stopPropagation()
+        let currSelected = e.target.parentElement.children.item(1).textContent.trim()
+        console.log(currSelected)
+        if (currSelected in legendMapRelations) {
+            legendMapRelations[currSelected]()
         }
+        e.stopPropagation()
     })
 
     //set the initial layer
@@ -187,60 +186,20 @@ fetch("../../Test_Data/full_table.json").then(response => response.json()).then(
 })
 
 //create an object to choose cases for whether a legend should be added or not
-
-
-
-// get geoJSON data and add markers to map
-
-// let USGS_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-// let tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
-
-// let counter = 0;
-
-// let colorCutoffDepths = [10, 30, 50, 70, 90]
-// let colorMap = { 0: "00FFFF", 1: "00FF00", 2: "80FF00", 3: "FFFF00", 4: "FF8000", 5: "FF0000"};
-
-// function setMarkers(data) {
-//     for (let i = 0; i < data.features.length; i++) {
-//         let curr_quake = data.features[i]
-//         let quake_time = new Date(curr_quake.properties["time"])
-//         let mag_scale = curr_quake.properties["mag"]
-//         let quake_location = curr_quake.properties["place"]
-//         let quake_depth = curr_quake.geometry.coordinates[2];
-
-//         let curr_color = colorMap[colorCutoffDepths.reduce((a, b, i) => b<quake_depth ? i+1 : a, 0)]
-
-//         let markerIcon = L.divIcon({
-//             html: `<svg width="${mag_scale * 10}" height="${mag_scale * 10}">
-//                                 <circle cx="${mag_scale * 5}" cy="${mag_scale * 5}" r="${mag_scale * 4}" stroke="black" stroke-width="1" fill=${"#"+curr_color} />
-//                                 </svg>`,
-//             iconAnchor: L.point([mag_scale * 5, mag_scale * 5]), 
-//             className: "circleIcon"
-//         });
-
-//         let curr_item = L.marker([curr_quake.geometry.coordinates[1], curr_quake.geometry.coordinates[0]], {icon: markerIcon})
-//         curr_item.bindPopup("Magnitude: " + mag_scale +"<br>" + 
-//                             "Depth: " + quake_depth + "<br>" +
-//                             "Location: " + quake_location + "<br>" +
-//                             "Time: " + quake_time.toString());
-
-//         //cleans data by removing invalid magnitudes (negative magnitudes are meaningless)
-//         if(mag_scale >= 0) {
-//             overlays[0]["Markers"].addLayer(curr_item)
-//         }   
-//     }
-// }
-
-// function addPlates(pl) {
-//     //now add the tectonic plate polygons
-//     for (let i = 0; i < pl.features.length; i++) {
-//         coords = pl.features[i].geometry.coordinates
-//         let curr_plate = L.polyline(coords, {weight: 3})
-//         overlays[0]["Tectonic Plates"].addLayer(curr_plate)
-//         console.log(i)
-//     }
-// }
-
+legendMapRelations = {
+    "startupCount": () => {console.log("Startup Count Selected!")},
+    "startupTotalValue": () => {
+        addLegend("Total Funding ($)", ["Hundreds of Thousands (10^5)",
+            "Millions (10^6)", "Tens of Millions (10^7)", "Hundreds of Millions (10^8)", "Billions (10^9)",
+            "Tens of Billions (10^10)", "Hundreds of Billions (10^11)"], totalFundingScale)
+    },
+    "startupAverageValue": () => {
+        addLegend("Average Startup Funding ($)", ["> Ten Thousand (10^4)",
+            "> Hundred Thousand (10^5)", "> Million (10^6)", "> Ten Million (10^7)", "> Hundred Million (10^8)"], 
+            avgFundingScale)
+    },
+    "percentageStartupCountWorldwide": () => {console.log("Company Count Percentage Selected!")}
+}
 
 
 // Add legend to map - function defined here
@@ -289,8 +248,8 @@ function addLegend(title, colorDepths, colors) {
     let colorSwatch = makeSwatch(colors[lastInd], 4)
 
 
-    let currLevel = document.createElement("div");
-    currLevel.textContent = ` > ${colorDepths[lastInd-1]}`
+    // let currLevel = document.createElement("div");
+    // currLevel.textContent = ` > ${colorDepths[lastInd-1]}`
     legend_el.appendChild(currLevel);
     currLevel.prepend(colorSwatch)
 
@@ -305,10 +264,4 @@ function removeLegend() {
     while(legend_el.children.length > 0) {
         legend_el.removeChild(legend_el.firstChild)
     }
-
-    // for (let i=0; i<legend_el.children.length; i++) {
-    //     legend_el.removeChild(legend_el.firstChild)
-    // }
-
-    console.log("success!")
 }
