c1faf7eb0b3389876404b12d74f6ba6b3acca0b1
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-09-28 09:59:53 -0700

    feat: Update app.py to connect to PostgreSQL database and return values from there rather than a static file

--- Webpages/Leaflet_Prototypes/app.py
+++ Webpages/Leaflet_Prototypes/app.py
@@ -1,10 +1,32 @@
 # import necessary libraries
+from sqlalchemy import inspect
 import os
-from flask import Flask, render_template, url_for, json
+from flask import Flask, render_template, url_for, json, jsonify
 import pandas as pd
+import config
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 # create instance of Flask app
 app = Flask(__name__)
+
+# connect to the postgres database
+connection_string = f'postgresql+psycopg2://{config.user}:{config.db_password}@localhost/vc_funding_db'
+
+db = create_engine(connection_string)
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(db, reflect=True)
+
+print([a for a in Base.classes])
+
+VC_Funding = Base.classes.vc_funding
+
+mapper = inspect(VC_Funding)
 
 
 # create route that renders index.html template
@@ -17,7 +39,20 @@ def samples():
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "Test_Data", "full_table.json")
     data = json.load(open(json_url))
-    return json.dumps(data)
+    return data
+
+@app.route("/api/city/<city>")
+def city_data(city):
+    session = Session(db)
+
+    results = session.query(VC_Funding).filter(VC_Funding.city == city).first()
+    #code from Stack Overflow - needed to know to use getattr instead of 
+    #bracket notation for sqlalchemy objects!
+    def get_model_dict(model):
+        return dict((column.name, getattr(model, column.name))
+                for column in model.__table__.columns)
+
+    return get_model_dict(results)
 
 if __name__ == "__main__":
     app.run(debug=True)
