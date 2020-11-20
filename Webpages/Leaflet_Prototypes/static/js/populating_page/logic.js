8ba04d67a9e31170c6b8aa00effc2f7b322d3813
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-11-20 07:47:16 -0800

    fix: Update maps to include new street maps from Mapbox - now load correctly!

--- Webpages/Leaflet_Prototypes/static/js/populating_page/logic.js
+++ Webpages/Leaflet_Prototypes/static/js/populating_page/logic.js
@@ -7,10 +7,10 @@ var myMap = L.map("map", {
 
 //make the layers for controlling the map
 var baseLayers = {
-    "Regular": L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    "Regular": L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        id: "mapbox.streets",
+        id: "mapbox.mapbox-streets-v8",
         accessToken: API_KEY
     })
 }
